6236afcd0ef5b7ef1a5edcb749c8ceaca268d875
ericywl <ericywl@live.com>
2018-11-29 16:25:20 +0800

    Add OCR for ticket reading

--- src/layouts/home/_TicketModal.js
+++ src/layouts/home/_TicketModal.js
@@ -1,56 +1,70 @@
 import React from 'react';
 import Modal from 'react-modal';
 
+import { parseTicket } from '../../util/ticket'
 import './_TicketModal.sass';
+const Tesseract = window.Tesseract;
 
 class TicketModal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.fileReader = null;
+  constructor(props) {
+    super(props);
+    this.state = {
+      ticket1: null,
+      ticket2: null
     }
+  }
 
-    componentWillMount() {
-        Modal.setAppElement('main');
-    }
+  componentWillMount() {
+    Modal.setAppElement('main');
+  }
 
-    handleFileChosen(file) {
-        this.fileReader = new FileReader();
-        this.fileReader.onload = function() {
-            let arr = new Uint8Array(this.result);
-            // TODO: Read the data and parse into JSON
-        }
-        this.fileReader.readAsArrayBuffer(file);
+  handleFileChosen(event) {
+    const file = event.target.files[0]
+    console.log(file.type)
+    if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
+      throw "Invalid file type"
     }
+    Tesseract.recognize(file).progress((status) => {
+      console.log(status);
+    }).then((data) => {
+      this.setState({ ticket1: parseTicket(data.text) });
+      console.log(this.state.ticket1);
+    });
+  }
 
-    render() {
-        return (
-            <Modal
-                isOpen={this.props.show}
-                contentLabel={'TicketModal'}
-                className='modal'
-                overlayClassName='modal-overlay'
-                onRequestClose={this.props.handleClose}
-                shouldFocusAfterRender={false}
-                shouldReturnFocusAfterClose={false}
-            >
-                <div className='modal-content'>
-                    <div className='modal-header'>
-                        <h2>Upload ticket PDF(s)</h2>
-                    </div>
-                    <div className='modal-body'>
-                        <div className='upload-box'>upload</div>
-                        <div className='upload-box'>test</div>
-                    </div>
-                    <div className='modal-footer'>
-                        <input
-                            type='file'
-                            onChange={ e => this.handleFileChosen(e.target.files[0]) }
-                        />
-                    </div>
-                </div>
-            </Modal>
-        )
-    }
+  render() {
+    return (
+      <Modal
+        isOpen={this.props.show}
+        contentLabel={'TicketModal'}
+        className='modal'
+        overlayClassName='modal-overlay'
+        onRequestClose={this.props.handleClose}
+        shouldFocusAfterRender={false}
+        shouldReturnFocusAfterClose={false}
+      >
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h2>Upload ticket PDF(s)</h2>
+          </div>
+          <div className='modal-body'>
+            <div className='upload-box'>
+              <div className='json'>
+                {!this.state.ticket1 ? 'ticket1' : JSON.stringify(this.state.ticket1)}
+              </div>
+            </div>
+            <div className='upload-box'>test</div>
+          </div>
+          <div className='modal-footer'>
+            <input
+              type='file'
+              onChange={this.handleFileChosen.bind(this)}
+            />
+          </div>
+        </div>
+      </Modal>
+    )
+  }
 }
 
 export default TicketModal
--- src/util/ticket.js
+++ src/util/ticket.js
@@ -1,58 +1,29 @@
-// Useless because pdfjs node_module doesn't seem to work on React
-// Probably gonna deprecate/remove this
+export const parseTicket = (text) => {
+  let arr = text.split('\n')
+  const resCode = arr[3].split(' ')[2]
+  const flightNum = arr[10].split(' ')[0]
+  const airline = arr[11].split(' ')[1]
+  const ticketClass = arr[12].split(' ')[1]
+  const airports1 = arr[7].split(' ').slice(1, 3)
+  const airports2 = arr[8].split(' ').slice(0, 2)
+  const departureAirport = airports1[0] + " " + airports2[0]
+  const arrivalAirport = airports1[1] + " " + airports2[1]
+  const departureDate = arr[6].split(' ').slice(1, 5).join(' ')
+  const tempArr = arr[14].split(' ')
+  const departureTime = tempArr[2].substr(0, 2) + ":" + tempArr[2].substr(3)
+  const arrivalTime = tempArr[3].substr(0, 2) + ":" + tempArr[3].substr(3)
+  const passengerName = arr[2]
 
-function pdf2Text(pdfFile) {
-    return PDFJS.getDocument(pdfFile).then(function(pdf) {
-        let pagePromise = pdf.getPage(1).then(function(page) {
-            return page.getTextContent().then(function(content) {
-                let text = ''
-                let lastY = -1;
-                content.items.forEach(function(item) {
-                    if (lastY !== item.transform[5]) {
-                        text += '\n'
-                        lastY = item.transform[5]
-                    }
-                    text += item.str.trim()
-                })
-                
-                return text
-            });
-        });
-        
-        return pagePromise.then(function(text) {
-            console.log(text)
-            return text;
-        });
-    });
-}
-
-export function parseTicket(pdfFile) {
-    return pdf2Text(pdfFile)
-    .then(function(text) {
-        let arr = text.split('\n')
-        let resCode = arr[4].split(' ')[2]
-        let flightNum = arr[9]
-        let airline = arr[10].split(': ')[1]
-        let ticketClass = arr[11].split(': ')[1]
-        let departureAirport = arr[13] + " " + arr[14]
-        let arrivalAirport = arr[15] + " " + arr[16]
-        let departureDate = arr[7].split('Please')[0]
-                                .split(':')[1]
-        let departureTime = arr[26]
-        let arrivalTime = arr[30]
-        let passengerName = arr[34].split('Check-in')[0]
-
-        return {
-            resCode,
-            flightNum,
-            airline,
-            ticketClass,
-            departureAirport,
-            departureDate,
-            departureTime,
-            arrivalAirport,
-            arrivalTime,
-            passengerName
-        }
-    })
+  return {
+    resCode,
+    flightNum,
+    airline,
+    ticketClass,
+    departureAirport,
+    departureDate,
+    departureTime,
+    arrivalAirport,
+    arrivalTime,
+    passengerName
+  }
 }
