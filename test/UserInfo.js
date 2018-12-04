03607b0e08a8b25ec9d5c68f42c4b84b830e091c
yaojie <limyaojie93@gmail.com>
2018-12-04 13:19:33 +0800

    add test for UserInfo

---
+++ test/UserInfo.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+require('truffle-test-utils').init();
+const UserInfo = artifacts.require('UserInfo');
+
+contract('UserInfo', async (accounts) => {
+  it('should create a new user when not created yet', async () => {
+    let instance = await UserInfo.deployed();
+    userExists = await instance.userExists();
+    assert.isFalse(userExists);
+
+    await instance.createUser();
+    userExists = await instance.userExists();
+    assert.isTrue(userExists);
+  });
+
+  it('should not allow re-creation of same user', async () => {
+    let instance = await UserInfo.deployed();
+    userExists = await instance.userExists();
+    assert.isTrue(userExists);
+
+    let err = null;
+    try {
+      await instance.createUser();
+    } catch (error) {
+      err = error
+    }
+    assert.ok(err instanceof Error)
+  });
+});
