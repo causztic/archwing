18c8b79da7da17c8501129d2b7ef966228149130
yaojie <limyaojie93@gmail.com>
2018-12-08 14:08:48 +0800

    update deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,11 +6,5 @@ const FlightValidity = artifacts.require("FlightValidity");
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(Coverage);
   deployer.deploy(FlightValidity);
-  deployer.deploy(ConversionRate).then(function() {
-    return ConversionRate.deployed();
-  }).then(function(cr) {
-    if (network == "production") {
-      cr.updateConversionToSGD();
-    }
-  });
+  deployer.deploy(ConversionRate);
 };
